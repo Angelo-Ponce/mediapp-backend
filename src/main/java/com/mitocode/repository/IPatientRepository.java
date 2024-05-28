1e21ef6996d4bacfff6fb3208439e907491cd53e
Angelo Ponce <angeloponcedev@gmail.com>
2024-05-27 21:33:55 -0500

    Add repository

---
+++ src/main/java/com/mitocode/repository/IPatientRepository.java
@@ -0,0 +1,7 @@
+package com.mitocode.repository;
+
+import com.mitocode.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPatientRepository extends JpaRepository<Patient, Integer> {
+}
